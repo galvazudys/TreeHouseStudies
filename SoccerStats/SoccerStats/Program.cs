abbdb890591e96fb8c0e86c5f728570d2c903424
golfa <golfa@LAPTOP-9QGEIVQK>
2017-01-21 15:06:26 +0000

    Add two lines of code to program

--- SoccerStats/SoccerStats/Program.cs
+++ SoccerStats/SoccerStats/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SoccerStats
 {
@@ -10,6 +11,8 @@ namespace SoccerStats
     {
         static void Main(string[] args)
         {
+            string currentDirctory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirctory);
         }
     }
 }
