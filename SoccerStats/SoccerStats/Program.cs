4f3c01f4e11ce215b9de656749f707ac19f7fdd6
golfa <golfa@LAPTOP-9QGEIVQK>
2017-01-22 11:41:01 +0000

    change some code to pull csv file and print in console

--- SoccerStats/SoccerStats/Program.cs
+++ SoccerStats/SoccerStats/Program.cs
@@ -13,21 +13,21 @@ namespace SoccerStats
         {
             string currentDirctory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirctory);
-            var fileName = Path.Combine(directory.FullName, "Data.txt");
-            var file = new FileInfo(fileName);
-            if (file.Exists)
-            {
-                using (var reader = new StreamReader(file.FullName))
-                {
-                    Console.SetIn(reader);
-                    Console.WriteLine(Console.ReadLine());
-                }
-
-
-            }
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadFile(fileName);
+            Console.WriteLine(fileContents);
 
 
 
         }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+        }
     }
 }
+
